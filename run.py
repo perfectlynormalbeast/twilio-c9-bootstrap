56dc2abf6d577772b75930f0d9b9605d81bda0a9
Vivek Nair <vivek@twilio.com>
2016-01-19 22:43:34 -0800

    Added skeleton Flask app Froze requirements

---
+++ run.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, redirect
+import twilio.twiml
+ 
+app = Flask(__name__)
+ 
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+ 
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
+
