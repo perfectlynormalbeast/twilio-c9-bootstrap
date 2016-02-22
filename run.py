a2ca9521660d4d0e4702109ee5b83dd1c6a751dd
Vivek Nair <vivek@twilio.com>
2016-02-22 00:03:29 -0800

    Remove app code for bootcamp

--- run.py
+++ run.py
@@ -2,14 +2,6 @@ from flask import Flask, request, redirect
 import twilio.twiml
 
 app = Flask(__name__)
- 
-@app.route("/sms", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
- 
-    resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
-    return str(resp)
- 
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
